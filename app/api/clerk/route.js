a12b3d59145bc0ae2b7e4ff5aba3704f840713d9
MohitKumar <K2004mohit@gmail.com>
2025-08-04 18:27:26 +0530

    update headers

--- app/api/clerk/route.js
+++ app/api/clerk/route.js
@@ -2,46 +2,45 @@ import { Webhook } from "svix";
 import ConnectDB from "@/config/db";
 import User from "@/models/User";
 import { headers } from "next/headers";
-import { NextRequest } from "next/server";
+import { NextResponse } from "next/server";
 
 export async function POST(req) {
     const wh = new Webhook(process.env.SIGNING_SECRET);
-    const headerPayload = await headers()
+    const headerPayload = headers();
     const svixHeaders = {
         "svix-id": headerPayload.get("svix-id"),
         "svix-timestamp": headerPayload.get("svix-timestamp"),
         "svix-signature": headerPayload.get("svix-signature"),
     };
 
-    //Get the payload and verify the signature
-    
+    // Get the payload and verify the signature
     const payload = await req.json();
     const body = JSON.stringify(payload);
     const { data, type } = wh.verify(body, svixHeaders);
-    
-    //Prepare the user data to be saved in the database
+
+    // Prepare the user data to be saved in the database
     const userData = {
         _id: data.id,
-        email: data.emailAddresses[0].emailAddresses,
-        name: `${data.firstName} ${data.lastName}`,
-        image: data.image_Url,
+        email: data.email_addresses?.[0]?.email_address || "",
+        name: `${data.first_name || ""} ${data.last_name || ""}`.trim(),
+        image: data.image_url || "",
     };
 
-    //Save the user data to the database
+    // Save the user data to the database
     await ConnectDB();
-    
+
     switch (type) {
         case "user.created":
             await User.create(userData);
             break;
         case "user.updated":
             await User.findByIdAndUpdate(data.id, userData);
-            break;  
+            break;
         case "user.deleted":
             await User.findByIdAndDelete(data.id);
             break;
         default:
             break;
     }
-    return NextRequest.json({ message: "Event received" });
+    return NextResponse.json({ message: "Event received" });
 }
