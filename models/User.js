6aee1852bbc52cc5cc2d67138fa2ea23172aeaac
MohitKumar <K2004mohit@gmail.com>
2025-08-04 18:38:48 +0530

    updates

--- models/User.js
+++ models/User.js
@@ -4,11 +4,11 @@ const UserSchema = new mongoose.Schema(
     {
         name: {type: String, required: true},
         email: {type: String, required: true},
-        password: {type: String, required: false}
+        password: {type: String, required: true}
     }, 
     {timestamps: true}
 );
 
-const User = mongoose.models.User || mongoose.model("User", UserSchema)
+const User = mongoose.models.User || mongoose.model("User", UserSchema);
 
 export default User;
